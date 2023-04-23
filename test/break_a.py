272a1dc1d47b22d8c64d08f181945df7ba96895d
LeiShe1 <664324441@qq.com>
2023-04-23 14:37:35 +0800

    first commit

---
+++ test/break_a.py
@@ -0,0 +1,14 @@
+
+
+
+for i in range(1000):
+    if i % 5 == 0 :
+        if i%10 == 0:
+            print(i)
+            break
+
+
+
+
+
+
---
+++ test/print_file.py
@@ -0,0 +1,27 @@
+
+import os
+import sys
+import numpy as np
+import x2ms_adapter
+# curPath = os.path.abspath(os.path.dirname(__file__))
+#
+# rootPath = os.path.split(curPath)[0]
+# print(curPath)
+# print(rootPath)
+#
+# # print(sys.path.append(rootPath))
+# print(sys.path)
+#
+# path = "/data/disk8T/data-home/shelei/data/cikm"
+# list_a = os.listdir(path)
+# list_a.sort()
+# print(list_a)
+#
+#
+# a = np.array([[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]])
+# print(a[:,14])
+#
+# print(os.path.dirname(__file__))
+
+for i in range(0,15):
+    print(i)
